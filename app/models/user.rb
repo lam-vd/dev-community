dc206e3b038a97c189935d324b52ba82e323f159
lamvd <vuduclam2412@gmail.com>
2024-12-25 17:46:36 +0700

    Element profile and FE header

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,18 @@ class User < ApplicationRecord
   def name
     "#{first_name} #{last_name}".strip
   end
+
+  def address
+    return if city.blank? || state.blank? || country.blank? || pincode.blank?
+    "#{city}, #{state}, #{country}, #{pincode}"
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ['country', 'city']
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root "home#index"
   get "up" => "rails/health#show", as: :rails_health_check
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get 'member/:id', to: 'members#show', as: :member
 end
